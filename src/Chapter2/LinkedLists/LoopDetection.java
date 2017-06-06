4330f28a3807ffe2411fb60fc95fad137fc112d8
Christina Nastevska <hristinanastevska@gmail.com>
2017-06-06 17:30:42 +0200

    detect cycle linkedlists

--- src/Chapter2/LinkedLists/LoopDetection.java
+++ src/Chapter2/LinkedLists/LoopDetection.java
@@ -12,5 +12,33 @@ package Chapter2.LinkedLists;
  */
 public class LoopDetection {
 
+    SLLNode<Integer> findBegining(SLLNode<Integer> head) {
+        SLLNode<Integer> slow = head;
+        SLLNode<Integer> fast = head;
+
+        /*Find meeting point. This will be LOOP_SIZE - k steps into the linked list. */
+        while (fast != null && fast.succ != null) {
+            slow = slow.succ;
+            fast = fast.succ.succ;
+            if (slow == fast) { // Collision point
+                break;
+            }
+        }
+        /*Error check - no meeting point, and therefore no loop*/
+        if (fast == null || fast.succ == null) return null;
+
+        /*
+          Move slow to Head. Keep fast at Meeting Point. Each are k steps from the
+          Loop Start. If they move at the same pace, they must meet at Loop Start.
+         */
+        slow = head;
+        while (slow != fast) {
+            slow = slow.succ;
+            fast = fast.succ;
+        }
+        /*Both now point to the start of the loop */
+        return fast;
+    }
+
 }
 
